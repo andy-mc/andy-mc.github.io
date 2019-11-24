40ba6b3795da60bbd2a6eb32c79c555bbe1f7eeb
_andy_mz <asmz5488@gmail.com>
2019-11-24 18:42:15 -0500

    create endpoints to refer to all html templates

--- server.py
+++ server.py
@@ -9,6 +9,26 @@ def favicon():
                                'airplane.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
+@app.route('/index.html')
 def home():
     return render_template('index.html')
-    
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/components.html')
+def components():
+    return render_template('components.html')
